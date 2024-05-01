9429a9f48d9e626584c97a41da419c9587afdd0b
Dave Earley <dave.m.earley@gmail.com>
2024-05-01 10:11:18 +0100

    add publishable Stripe key

--- digitalocean-start.sh
+++ digitalocean-start.sh
@@ -2,7 +2,6 @@
 export VITE_FRONTEND_URL=${APP_FRONTEND_URL:-"/"}
 
 echo "Starting with the following environment variables:"
-echo "APP_FRONTEND_URL=${APP_FRONTEND_URL}"
 echo "VITE_FRONTEND_URL=${VITE_FRONTEND_URL}"
 
 exec /startup.sh
